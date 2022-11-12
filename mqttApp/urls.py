9c3120c9d6f044ef9582bc0cdf3a3735f54d5bad
CristinoCg <cristinocanga9@gmail.com>
2022-11-12 10:48:22 +0100

    update
    Separei as views e subscribes do mqtt

--- mqttApp/urls.py
+++ mqttApp/urls.py
@@ -1,6 +1,23 @@
-from django.urls import include, path
-from .views import HomePageView
+from django.urls import path
+from .views import (
+    HomePageView, 
+    TemperaturaView, 
+    BatimentoView,
+    OxigenioView,
+    LocalizacaoView,
+    ClienteView)
 
 urlpatterns=[
     path('', HomePageView, name='home'),
+    path('paciente/', ClienteView, name='paciente'),
+    
 ]
+
+broker_ulrpatterns=[
+    path('batimento', BatimentoView, name='batimento'),
+    path('temperatura', TemperaturaView, name='temperatura'),
+    path('localizacao', LocalizacaoView, name='localizacao'),
+    path('oxigenio', OxigenioView, name='oxigenio')
+
+]
+urlpatterns += broker_ulrpatterns
--- mqttApp/views.py
+++ mqttApp/views.py
@@ -1,23 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 import paho.mqtt.client as mqttClient
 import json
 import logging
 import asyncio
+from random import choice
 
 #logging.disable(logging.WARNING)
-
 logging.basicConfig(level=logging.DEBUG,format='%(levelname)s - %(asctime)s - %(message)s')
-
-
+lista = list('BROWNFOX')
 conteudo = ''
+context = {'/batimento':'','/temperatura':'','/oxigenio':'','/localizacao':''}
+flag = {'/batimento':False,'/temperatura':False,'/oxigenio':False,'/localizacao':False}
 # Create your views here.
-def HomePageView(request):
-    
+def HomePageView(request): 
+    return render(request, 'home.html',{'conteudo': context}) 
+
+
+def BrokerView(request):
+    global lista, flag
+    response_path = request.path
+#    logging.debug(f'Res:{response_path}')
     async def cliente_assincrono():
         
-        cliente = mqttClient.Client('django_client')
-
-        
+        cliente = mqttClient.Client('django_client')      
         def on_connect(client, userdata, flags, rc):
             if rc==0:
                 client.connected_flag=True #set flag
@@ -26,51 +33,71 @@ def HomePageView(request):
                 print("Bad connection Returned code=",rc)
             
         def on_message(client, userdata, message):
-            global conteudo
+            global conteudo, context
             conteudo = str(message.payload.decode('utf-8'))
-            ponto = conteudo.find(':') + 1
+            #ponto = conteudo.find(':') + 1
+            context[response_path] = conteudo
             if message.retain:
-                ...
+                logging.debug('This one is retained')
             else:
-                ...
-            logging.debug(f'Valor de conteudo, dentor do message:{conteudo[ponto:]}')
-            logging.debug(str(message.payload.decode('utf-8')))
-            return render(request, 'home.html',{'conteudo': conteudo[ponto:]})
-            
+                logging.debug(f'Message:{context[response_path]} - URL{response_path}')
 
         def on_subscribe(client, userdata, mid, granted_qos):
-            logging.debug(f'Resultado:{userdata}')
-            
-            
+            logging.debug(f'Dados do usuário subscribe:{userdata}')
+                   
         def on_disconnect(client, userdata, rc=0):
             client.loop_stop()
+
+        
     
-        broker = 'broker.hivemq.com'#'afdf7a62ec4f41598f7116d5ad52aa26.s1.eu.hivemq.cloud'
-        porta = 1883#8883
+        broker = 'broker.hivemq.com'
+        porta = 1883
         
         cliente.on_connect = on_connect
         cliente.on_message = on_message
         cliente.on_disconnect = on_disconnect
         cliente.on_subscribe = on_subscribe
-        
-        
-        
-        
+        '''if not flag[response_path]:
+            flag[response_path] = True'''
         try:
             cliente.connect(broker,porta)
-            print('Conectou')
-            cliente.subscribe('teste/')
+            logging.debug(f'Res:{response_path}')
+            cliente.subscribe(response_path)
             cliente.loop_start()
         except Exception as exception:
             print('Chegou até aqui', exception)
-            return render(request, 'home.html',{'conteudo': 'Não funcionou, ainda:)'})
-
-    #print(conteudo)  
+            return HttpResponse('Nada')
+            
+    
     async def main():
         tarefa = asyncio.create_task(cliente_assincrono())    
     asyncio.run(main())
-    return render(request, 'home.html',{'conteudo': conteudo})
 
+    return HttpResponse(context[response_path])
+
+@csrf_protect
+def BatimentoView(request):
+    logging.debug(f'{context}')
+    return BrokerView(request)
+
+@csrf_protect
+def TemperaturaView(request):
+    logging.debug(f'{context}')
+    return BrokerView(request)
+
+@csrf_protect
+def OxigenioView(request):
+    logging.debug(f'{context}')
+    return BrokerView(request)
+
+@csrf_protect
+def LocalizacaoView(request):
+    logging.debug(f'{context}')
+    return BrokerView(request)
+
+def ClienteView(request):
+    return render(request, 'paciente.html')
+    
 
 
     
